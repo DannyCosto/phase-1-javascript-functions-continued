7c4ba3905d244b4968b5a0e9a67e1b577dd3bf05
Danny <danny.costelloe@gmail.com>
2021-03-16 20:47:45 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+
+}
+
+function mondayWork(act="go to the office"){
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(param1="*"){
+    return function(param2="special"){
+        return `You are ${param1}${param2}${param1}!`
+    }
+}
